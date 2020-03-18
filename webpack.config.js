d6b0b00ff4e76ec2ed1e423187000a4f4bdb38c8
hfq <373366090@qq.com>
2020-03-18 16:31:08 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+//const path = require('path')
+
+//导入在内存中自动生成index页面的插件
+//const htmlWebpackPlugin = require('html-webpack-plugin')  
+
+//创建一个插件的实例对象
+// const htmlPlugin = new htmlWebpackPlugin({
+//     template: path.join(__dirname,'./src/index.html'),//源文件
+//     filename: 'index.html' //生成的内存中首页的名称
+// })
+
+module.exports = {
+    mode: "development"
+    
+    // plugins: [
+    //     htmlPlugin
+    // ]
+}
